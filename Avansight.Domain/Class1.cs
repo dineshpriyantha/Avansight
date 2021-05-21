43002be63f80a230a7b9219b1eaa613eabfe3d04
Dinesh Priyantha <dinesh.priyantha.uom@gmail.com>
2021-05-22 00:14:25 +0530

    Add new classes Patient and TreatmentReading

--- Avansight.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Avansight.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ Avansight.Domain/Patient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Avansight.Domain
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        [Range(20, 100)]
+        public int Age { get; set; }
+        public string Gender { get; set; }
+    }
+}
