185904dd9497eca2872acfcdea0bb73e359e315a
Dinesh Priyantha <dinesh.priyantha.uom@gmail.com>
2021-05-23 01:33:59 +0530

    Update PatientController logic

--- Avansight/Controllers/PatientController.cs
+++ Avansight/Controllers/PatientController.cs
@@ -40,32 +40,32 @@ namespace Avansight.Controllers
             int totalNoOfPatientInEachAgeRange = ageGroup2130 + ageGroup3140 + ageGroup4150 + ageGroup5160;
 
             //  logic to generate records.
-            int noOfMales = 0;
-            int noOfFemales = 0;
-            noOfMales = (maleWeight / (maleWeight + femaleWeight)) * sampleSize;
-            noOfFemales = sampleSize - noOfMales;
+            //int noOfMales = 0;
+            //int noOfFemales = 0;
+            int noOfMales = (maleWeight * sampleSize / (maleWeight + femaleWeight)) ;
+            int noOfFemales = sampleSize - noOfMales;
 
-            int noOfMalesin2130ageRange = (ageGroup2130 / (ageGroup2130 + totalNoOfPatientInEachAgeRange)) * noOfMales;
-            int noOfFemaleIn2130agerange = (ageGroup2130 / (ageGroup2130 + totalNoOfPatientInEachAgeRange)) * noOfFemales;
+            int noOfMalesin2130ageRange = ageGroup2130 * noOfMales / (ageGroup2130 + totalNoOfPatientInEachAgeRange);
+            int noOfFemaleIn2130agerange = ageGroup2130 * noOfFemales / (ageGroup2130 + totalNoOfPatientInEachAgeRange);
 
-            int noOfMalesin3140ageRange = (ageGroup3140 / (ageGroup3140 + totalNoOfPatientInEachAgeRange)) * noOfMales;
-            int noOfFemaleIn3140agerange = (ageGroup3140 / (ageGroup3140 + totalNoOfPatientInEachAgeRange)) * noOfFemales;
+            int noOfMalesin3140ageRange = ageGroup3140 * noOfMales / (ageGroup3140 + totalNoOfPatientInEachAgeRange);
+            int noOfFemaleIn3140agerange = ageGroup3140 * noOfFemales / (ageGroup3140 + totalNoOfPatientInEachAgeRange);
 
-            int noOfMalesin4150ageRange = (ageGroup4150 / (ageGroup4150 + totalNoOfPatientInEachAgeRange)) * noOfMales;
-            int noOfFemaleIn4150agerange = (ageGroup4150 / (ageGroup4150 + totalNoOfPatientInEachAgeRange)) * noOfFemales;
+            int noOfMalesin4150ageRange = ageGroup4150 * noOfMales / (ageGroup4150 + totalNoOfPatientInEachAgeRange);
+            int noOfFemaleIn4150agerange = ageGroup4150 * noOfFemales / (ageGroup4150 + totalNoOfPatientInEachAgeRange);
 
-            int noOfMalesin5160ageRange = (ageGroup5160 / (ageGroup5160 + totalNoOfPatientInEachAgeRange)) * noOfMales;
-            int noOfFemaleIn5160agerange = (ageGroup5160 / (ageGroup5160 + totalNoOfPatientInEachAgeRange)) * noOfFemales;
+            int noOfMalesin5160ageRange = ageGroup5160 * noOfMales / (ageGroup5160 + totalNoOfPatientInEachAgeRange);
+            int noOfFemaleIn5160agerange = ageGroup5160 * noOfFemales / (ageGroup5160 + totalNoOfPatientInEachAgeRange);
                         
-            ListOfMalePatient(noOfMalesin2130ageRange, "Male", 21, 30);
-            ListOfMalePatient(noOfMalesin3140ageRange, "Male", 31, 40);
-            ListOfMalePatient(noOfMalesin4150ageRange, "Male", 41, 50);
-            ListOfMalePatient(noOfMalesin5160ageRange, "Male", 51, 60);
+            ListOfMalePatient(noOfMalesin2130ageRange, "M", 21, 30);
+            ListOfMalePatient(noOfMalesin3140ageRange, "M", 31, 40);
+            ListOfMalePatient(noOfMalesin4150ageRange, "M", 41, 50);
+            ListOfMalePatient(noOfMalesin5160ageRange, "M", 51, 60);
 
-            ListOfMalePatient(noOfFemaleIn2130agerange, "FeMale", 21, 30);
-            ListOfMalePatient(noOfFemaleIn3140agerange, "FeMale", 31, 40);
-            ListOfMalePatient(noOfFemaleIn4150agerange, "FeMale", 41, 50);
-            ListOfMalePatient(noOfFemaleIn5160agerange, "FeMale", 51, 60);
+            ListOfMalePatient(noOfFemaleIn2130agerange, "F", 21, 30);
+            ListOfMalePatient(noOfFemaleIn3140agerange, "F", 31, 40);
+            ListOfMalePatient(noOfFemaleIn4150agerange, "F", 41, 50);
+            ListOfMalePatient(noOfFemaleIn5160agerange, "F", 51, 60);
 
 
             // Unit of work
