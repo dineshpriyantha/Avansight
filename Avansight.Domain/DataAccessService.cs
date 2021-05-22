9fda416530eff55ca49d0c1b1f868e5f516135e8
Dinesh Priyantha <dinesh.priyantha.uom@gmail.com>
2021-05-22 07:36:21 +0530

    DataAccessService updated, added new patient view

--- Avansight.Domain/DataAccessService.cs
+++ Avansight.Domain/DataAccessService.cs
@@ -1,10 +1,98 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 
 namespace Avansight.Domain
 {
-    class DataAccessService
+    public interface IDapper : IDisposable
     {
+        DbConnection GetDbconnection();
+        T Get<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        IEnumerable<T> GetAll<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        int Execute(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Insert<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Update<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+    }
+    public class DataAccessService : IDapper
+    {
+        public static IConfiguration _config { get; private set; }
+        private string ConnectionString = _config["ConnectionStrings:DbConnection"];
+
+        public DataAccessService(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Execute(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            using IDbConnection db = new SqlConnection(_config.GetConnectionString(ConnectionString));
+            return db.Query<T>(sp, parms, commandType: commandType).FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            using IDbConnection db = new SqlConnection(_config.GetConnectionString(ConnectionString));
+            return db.Query<T>(sp, parms, commandType: commandType).ToList();
+        }
+
+        public DbConnection GetDbconnection()
+        {
+            return new SqlConnection(_config.GetConnectionString(ConnectionString));
+        }
+
+        public T Insert<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            T result;
+            using IDbConnection db = new SqlConnection(_config.GetConnectionString(ConnectionString));
+            try
+            {
+                if (db.State == ConnectionState.Closed)
+                    db.Open();
+
+                using var tran = db.BeginTransaction();
+
+                try
+                {
+                    result = db.Query<T>(sp, parms, commandType: commandType, transaction: tran).FirstOrDefault();
+                    tran.Commit();
+                }
+                catch (Exception)
+                {
+                    tran.Rollback();
+                    throw;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (db.State == ConnectionState.Open)
+                    db.Close();
+            }
+
+            return result;
+        }
+
+        public T Update<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
